aee0ee5cfbb1d4aa87f185e902cc244b4cb09e3d
ajinkyakambe <ajinkyakambe@gmail.com>
2022-09-27 16:24:42 +0530

    Added UC1: check present or absent

---
+++ src/com/bridgelabz/EmployeeWage.java
@@ -0,0 +1,13 @@
+package com.bridgelabz;
+
+public class EmployeeWage {
+
+    // some logic
+    //UC1  Check Employee is
+    //Present or Absent
+    //- Use ((RANDOM)) for Attendance
+    //Check
+
+
+
+}
